4f421967050ff6effc1cc1a9138f0d2302944eb5
Borgeshc <Borgeshc@gmatc.matc.edu>
2018-01-19 13:42:22 -0600

    Added explosions and effects

---
+++ Assets/Game/Scripts/Limb.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limb : MonoBehaviour
+{
+    public int limbHealth = 150;
+    public bool canFall;
+    public bool addforce;
+    public GameObject explosion;
+
+    int health;
+    bool isDead;
+
+    Health baseHealth;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        health = limbHealth;
+        baseHealth = transform.root.GetComponent<Health>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void TookDamage(int damage)
+    {
+        health -= damage;
+        baseHealth.TookDamage(damage);
+
+        if (health <= 0 && !isDead)
+        {
+            isDead = true;
+            Died();
+        }
+    }
+
+    void Died()
+    {
+        baseHealth.LimbDestroyed();
+
+        Instantiate(explosion, transform.position, Quaternion.identity);
+
+        if(canFall)
+        {
+            transform.parent = null;
+            rb.isKinematic = false;
+            rb.useGravity = true;
+
+            if (addforce)
+                rb.AddForce(-transform.forward * 5, ForceMode.Impulse);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
